907b018fb1d0099a38b36d6d97d62036a9d8d049
tmoiseev <verront@gmail.com>
2018-04-03 13:22:48 +0300

    Small fix

--- app/controllers/TaskController.js
+++ app/controllers/TaskController.js
@@ -23,7 +23,6 @@ exports.getTaskById = function (req, res) {
 exports.getTaskByKeyword = function (req, res) {
     var keyword = req.params.keyword;
     TaskDao.getTaskByKeyword(keyword).then(function (tasks) {
-        console.log(tasks);
         if (tasks.length > 0) {
             return res.send({error: false, data: tasks});
         } else {
